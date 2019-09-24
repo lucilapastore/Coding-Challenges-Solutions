03e94c737f6ce1483c3b47e05499b98e3cab45f4
Lucila Pastore <52759155+lucilapastore@users.noreply.github.com>
2019-09-24 14:15:31 -0300

    Update Library Fine.js

--- Library Fine.js
+++ Library Fine.js
@@ -2,6 +2,21 @@ function libraryFine(d1, m1, y1, d2, m2, y2) {
       let fine = 0;
     if (y1 > y2) {
         fine = 10000;
+    } else if (y1 === y2 && m1 > m2) {
+        fine = 500 * (m1 - m2);
+    } else if (y1 === y2 && m1 === m2 && d1 > d2) {
+        fine = 15 * (d1 - d2);
+    } else {
+        fine;
+    }
+    return fine;
+}
+
+
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+      let fine = 0;
+    if (y1 > y2) {
+        fine = 10000;
         return fine;
     }
     if (y1 === y2 && m1 > m2) {
